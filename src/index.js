7b4bf5543ca47fc2714540d3a56eeecdbf7eb58e
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2018-12-31 12:53:27 +0300

    separate parsers code frome main logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { union, has } from 'lodash';
 import { readFileSync } from 'fs';
+import { extname } from 'path';
+import parse from './parsers';
 
 const beforeView = (key, value) => `  - ${key}: ${value}`;
 const afterView = (key, value) => `  + ${key}: ${value}`;
@@ -18,13 +20,18 @@ const diffCalc = (k, b, a) => {
   return [afterView(k, a[k]), beforeView(k, b[k])].join('\n');
 };
 
-const plainJson = (before, after) => {
-  const beforeData = readFileSync(before, 'utf-8');
-  const afterData = readFileSync(after, 'utf-8');
-  const beforeObject = JSON.parse(beforeData);
-  const afterObject = JSON.parse(afterData);
+const getFileType = filePath => extname(filePath).toLowerCase().slice(1);
+
+const diff = (before, after) => {
+  const beforeType = getFileType(before);
+  const afterType = getFileType(after);
+  const beforeRawData = readFileSync(before, 'utf-8');
+  const afterRawData = readFileSync(after, 'utf-8');
+  const beforeObject = parse(beforeRawData, beforeType);
+  const afterObject = parse(afterRawData, afterType);
   const diffStr = union(Object.keys(beforeObject), (Object.keys(afterObject)))
     .map(k => diffCalc(k, beforeObject, afterObject));
   return ['{', ...diffStr, '}'].join('\n');
 };
-export default plainJson;
+
+export default diff;
