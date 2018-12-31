c2b4d36e5bf698ac71a2b9c7ebfca288c2d8a5e9
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2018-12-31 11:17:40 +0300

    add action to bin

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,10 +1,21 @@
 #!/usr/bin/env node
 import gendiff from 'commander';
 import { version } from '../../package.json';
+import plainJson from '..';
 
 gendiff
   .version(version)
   .description('Compares two configuration files and shows a difference.')
   .arguments('<firstConfig> <secondConfig>')
   .option('-f, --format [type]', 'Output format')
+  .action((firstConfig, secondConfig) => {
+    try {
+      const result = plainJson(firstConfig, secondConfig);
+      console.log(result);
+    } catch (err) {
+      const { errno, message } = err;
+      console.error(message);
+      process.exit(errno);
+    }
+  })
   .parse(process.argv);
