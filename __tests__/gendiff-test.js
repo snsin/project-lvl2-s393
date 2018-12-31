919178bc271490e26174b4d3dcf4d80537a0bde5
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2018-12-31 12:47:58 +0300

    implement tests for .yaml and .confrc-like files

--- __tests__/gendiff-test.js
+++ __tests__/gendiff-test.js
@@ -1,9 +1,22 @@
-import fs from 'fs';
+import path from 'path';
+import { readFileSync } from 'fs';
 import genDiff from '../src';
 
-test('simple diff', () => {
-  const before = '__tests__/__fixtures__/before.json';
-  const after = '__tests__/__fixtures__/after.json';
-  const data = fs.readFileSync('__tests__/__fixtures__/b-a-diff.txt', 'utf8');
-  expect(genDiff(before, after)).toBe(data);
-});
+const getDiffTest = (before, after, expected) => {
+  const fixtureDir = '__tests__/__fixtures__';
+  const beforePath = path.join(fixtureDir, before);
+  const afterPath = path.join(fixtureDir, after);
+  const expectedData = readFileSync(path.join(fixtureDir, expected), 'utf-8');
+  return () => expect(genDiff(beforePath, afterPath)).toBe(expectedData);
+};
+
+test('".confrc"-like config', getDiffTest('.beforerc', 'after.json', 'b-a-diff.txt'));
+
+
+['.json', '.yaml', '.YML'].forEach(ext => test(`pretty format, plain with ${ext}`, () => {
+  const fixtureDir = '__tests__/__fixtures__';
+  const beforePath = path.join(fixtureDir, `before${ext}`);
+  const afterPath = path.join(fixtureDir, `after${ext}`);
+  const expectedData = readFileSync(path.join(fixtureDir, 'b-a-diff.txt'), 'utf-8');
+  expect(genDiff(beforePath, afterPath, 'plain')).toBe(expectedData);
+}));
