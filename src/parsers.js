92f1e8f5670e5f9534a3da1e5c070fa76584b5fe
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2019-01-01 19:16:27 +0300

    add .ini files support

--- src/parsers.js
+++ src/parsers.js
@@ -1,7 +1,9 @@
 import { has } from 'lodash';
 import yamlParser from 'js-yaml';
+import iniParser from 'ini';
 
 const selector = {
+  ini: iniParser.decode,
   yaml: yamlParser.safeLoad,
   yml: yamlParser.safeLoad,
   json: JSON.parse,
