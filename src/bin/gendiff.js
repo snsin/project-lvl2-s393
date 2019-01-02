219cbb23126d3092536af23ac270263ed8f63749
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2019-01-02 20:56:27 +0300

    added default output format (pretty)

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,13 +3,18 @@ import gendiff from 'commander';
 import { version } from '../../package.json';
 import createDiff from '..';
 
+const defaultFormat = 'pretty';
+
 gendiff
   .version(version)
   .description('Compares two configuration files and shows a difference.')
   .arguments('<firstConfig> <secondConfig>')
-  .option('-f, --format [type]', 'Output format')
+  .option('-f, --format [type]', `Output format[${defaultFormat}]`, [defaultFormat])
   .action((firstConfig, secondConfig) => {
     try {
+      if (!defaultFormat.includes(gendiff.format)) {
+        console.warn(`${gendiff.format} is not supported. Used default (${defaultFormat})`);
+      }
       const result = createDiff(firstConfig, secondConfig);
       console.log(result);
     } catch (err) {
