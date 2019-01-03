af5bba4bf76a6c9f0459bafb84794a2209de1976
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2019-01-03 10:32:02 +0300

    implement format selection

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -2,20 +2,21 @@
 import gendiff from 'commander';
 import { version } from '../../package.json';
 import createDiff from '..';
+import { defaultRenderer, rendererSelector } from '../renderers';
 
-const defaultFormat = 'pretty';
+const list = Object.keys(rendererSelector).join('|');
 
 gendiff
   .version(version)
   .description('Compares two configuration files and shows a difference.')
   .arguments('<firstConfig> <secondConfig>')
-  .option('-f, --format [type]', `Output format[${defaultFormat}]`, [defaultFormat])
+  .option('-f, --format [type]', `Output format[${list}]`, `${defaultRenderer}`)
   .action((firstConfig, secondConfig) => {
     try {
-      if (!defaultFormat.includes(gendiff.format)) {
-        console.warn(`${gendiff.format} is not supported. Used default (${defaultFormat})`);
+      if (!list.includes(gendiff.format)) {
+        console.warn(`${gendiff.format} is not supported. Used default (${defaultRenderer})`);
       }
-      const result = createDiff(firstConfig, secondConfig);
+      const result = createDiff(firstConfig, secondConfig, gendiff.format);
       console.log(result);
     } catch (err) {
       const { errno, message } = err;
