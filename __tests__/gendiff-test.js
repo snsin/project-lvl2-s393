a6d96edfdb9d0c10ff7d74ba6b31fea928a2333c
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2019-01-03 08:42:33 +0300

    recognize output format in tests by special files naming

--- __tests__/gendiff-test.js
+++ __tests__/gendiff-test.js
@@ -11,11 +11,15 @@ test.each([
   ['nested-before.json', 'nested-after.json', 'nested-b-a-diff.txt'],
   ['nested-before.yaml', 'nested-after.yaml', 'nested-b-a-diff.txt'],
   ['nested-before.ini', 'nested-after.ini', 'nested-b-a-diff.txt'],
+  ['nested-before.json', 'nested-after.json', 'nested-b-a-diff.plain.txt'],
+  ['nested-before.yaml', 'nested-after.yaml', 'nested-b-a-diff.plain.txt'],
+  ['nested-before.ini', 'nested-after.ini', 'nested-b-a-diff.plain.txt'],
 ])('test %s and %s difference',
   (before, after, expected) => {
     const fixtureDir = '__tests__/__fixtures__';
     const beforePath = path.join(fixtureDir, before);
     const afterPath = path.join(fixtureDir, after);
+    const format = path.basename(expected, 'txt').split('.')[1] || 'pretty';
     const expectedData = readFileSync(path.join(fixtureDir, expected), 'utf-8');
-    expect(createDiff(beforePath, afterPath)).toBe(expectedData);
+    expect(createDiff(beforePath, afterPath, format)).toBe(expectedData);
   });
