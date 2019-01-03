99a5c4a01f90dd1544ec7a3f3f83660364021671
Sergey Sinitsyn <s.n.sinitsyn@gmail.com>
2019-01-03 10:21:05 +0300

    implement plain renderer and renderer selection

--- src/renderers.js
+++ src/renderers.js
@@ -1,7 +1,18 @@
-import { flatten } from 'lodash';
+import { flatten, has } from 'lodash';
 
 const getKeyIndent = level => `${' '.repeat(4 * level + 2)}`;
 const getBraceIndent = level => `${' '.repeat(4 * (level + 1))}`;
+
+const getKeyView = (parentKey, key) => (parentKey ? `${parentKey}.${key}` : `${key}`);
+const getValView = (val) => {
+  if (typeof val === 'string') {
+    return `'${val}'`;
+  }
+  if (val instanceof Object) {
+    return '[complex value]';
+  }
+  return val;
+};
 
 const stringify = (val, indentLevel) => {
   if (val instanceof Object) {
@@ -16,7 +27,7 @@ const stringify = (val, indentLevel) => {
   return val;
 };
 
-const renderTree = (tree, indentLevel = 0) => {
+const renderPretty = (tree, indentLevel = 0) => {
   const keyIndent = getKeyIndent(indentLevel);
   const { key, type, children } = tree;
   switch (type) {
@@ -34,7 +45,7 @@ const renderTree = (tree, indentLevel = 0) => {
     case 'nested':
       return flatten([
         `${keyIndent}  ${key}: {`,
-        children.map(child => renderTree(child, indentLevel + 1)),
+        children.map(child => renderPretty(child, indentLevel + 1)),
         `${getBraceIndent(indentLevel)}}`,
       ]).join('\n');
     default:
@@ -42,4 +53,47 @@ const renderTree = (tree, indentLevel = 0) => {
   }
 };
 
-export default tree => ['{', ...tree.map(child => renderTree(child, 0)), '}'].join('\n');
+const renderPlain = (tree, parentKey = '') => {
+  const { key, type } = tree;
+  const property = getKeyView(parentKey, key);
+  switch (type) {
+    case 'changed':
+      return [
+        `Property '${property}' was updated.`,
+        `From ${getValView(tree.oldValue)} to ${getValView(tree.newValue)}`,
+      ].join(' ');
+
+    case 'added':
+      return `Property '${property}' was added with value: ${getValView(tree.value)}`;
+
+    case 'deleted':
+      return `Property '${property}' was removed`;
+
+    case 'unchanged':
+      return '';
+
+    case 'nested':
+      return flatten(tree.children.map(child => renderPlain(child, property)))
+        .filter(v => v)
+        .join('\n');
+
+    default:
+      throw new Error('Unexpected node type');
+  }
+};
+
+export const defaultRenderer = 'pretty';
+export const rendererSelector = {
+  [defaultRenderer]: tree => [
+    '{',
+    ...tree.map(child => renderPretty(child, 0)),
+    '}',
+  ].join('\n'),
+  plain: tree => tree.map(node => renderPlain(node, '')).join('\n'),
+};
+
+export default (data, format) => {
+  const render = has(rendererSelector, format)
+    ? rendererSelector[format] : rendererSelector[defaultRenderer];
+  return render(data);
+};
